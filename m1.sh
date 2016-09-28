5ee9fb468c18eed4c0e96725cc6430f0f1472230
yange <yange@apple.com>
2016-09-28 11:31:42 +0800

    Simple availability check and alert.

---
+++ m1.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+pattern='plus128j\">✓<'
+url="https://www.m1.com.sg/micro/iphone/stock"
+now=$(date "+%Y.%m.%d-%H.%M.%S")
+curl $url > test.html
+
+if grep $pattern test.html
+	then
+		/usr/bin/osascript <<EOT
+		tell application "Messages"
+			send "Found iphone 7 Plus 128G Jet Black at $url" to buddy "yangzhennanedward@gmail.com" of (service 1 whose service type is iMessage)
+		end tell
+EOT
+exit 0
+else
+	echo "Not found. -- $now">> log.txt
+fi
+exit 0
